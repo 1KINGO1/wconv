94f90aa03138b3777b16ac120f1644231df03136
KINGO <spental12345@gmail.com>
2025-06-04 09:41:16 +0300

    Added recent conversions on the home page

--- frontend/src/features/conversion/dashboard/RecentConversions.tsx
+++ frontend/src/features/conversion/dashboard/RecentConversions.tsx
@@ -1,37 +1,32 @@
+'use client'
+
 import { ConversionItem } from '@/features/conversion/ConversionItem'
 import { ConversionFormat } from '@/shared/constants/conversion-format'
 import { ConversionState } from '@/shared/constants/conversion-state'
+import { useConversions } from '@/features/conversion/hooks/useConversions'
+import { getFileDownloadUrl } from '@/shared/utils/get-file-download-url'
 
 export function RecentConversions() {
+  const {data, isSuccess} = useConversions();
+
   return (
     <div className='mt-24 text-center'>
       <h1 className='text-3xl'>Recent Conversions</h1>
       <div className='grid grid-cols-1 auto-rows-fr gap-4 mt-6'>
-        <ConversionItem
-          state={ConversionState.PENDING}
-          fileFromName='3312312451242_aboba.jpg'
-          fileFromUrl='http://localhost:3000/api/conversion/files/3312312451242_aboba.png'
-          fileFromFormat={ConversionFormat.JPG}
-          fileToFormat={ConversionFormat.PNG}
-        />
-        <ConversionItem
-          state={ConversionState.SUCCESS}
-          fileFromName='3312312451242_aboba.jpg'
-          fileFromUrl='http://localhost:3000/api/conversion/files/3312312451242_aboba.png'
-          fileFromFormat={ConversionFormat.JPG}
-          fileToFormat={ConversionFormat.PNG}
-          fileToName={'converted_3312312451242_aboba.jpg'}
-          fileToUrl={
-            'http://localhost:3000/api/conversion/files/3312312451242_aboba.png'
-          }
-        />
-        <ConversionItem
-          state={ConversionState.FAILED}
-          fileFromName='3312312451242_aboba.jpg'
-          fileFromUrl='http://localhost:3000/api/conversion/files/3312312451242_aboba.png'
-          fileFromFormat={ConversionFormat.JPG}
-          fileToFormat={ConversionFormat.PNG}
-        />
+        {isSuccess && (
+          data.slice(0,3).map((conversion) => (
+            <ConversionItem
+              key={conversion.id}
+              state={conversion.state}
+              fileFromName={conversion.fileFromName}
+              fileFromUrl={getFileDownloadUrl(conversion.fileFromName)}
+              fileFromFormat={conversion.fileFromFormat}
+              fileToFormat={conversion.fileToFormat}
+              fileToName={conversion.fileToName}
+              fileToUrl={getFileDownloadUrl(conversion.fileToName)}
+            />
+          ))
+        )}
       </div>
     </div>
   )
